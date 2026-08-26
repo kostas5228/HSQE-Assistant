efe0e8f4eb0573ba9634e4b2f0268146e5b701e4
kostas5228 <k.kordopatis@outlook.com>
2026-08-26 10:54:24 +0300

    Increase column width for date in Inspections page

--- hsqe-assistant-frontend/src/pages/Inspections.jsx
+++ hsqe-assistant-frontend/src/pages/Inspections.jsx
@@ -1690,7 +1690,7 @@ export default function Inspections() {
 
   function colWidth(key) {
     const map = {
-      date: 90,
+      date: 100,
       vessel: 120,
       inspection_type: 190,
       place: 170,
